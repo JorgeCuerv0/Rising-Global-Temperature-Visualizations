c4b5f9d47d0b5a925dee5e1601dafbf00c00c8d0
Tim <timothym98@ischool.berkeley.edu>
2024-07-07 18:29:32 -0400

    Website Skeleton
    Created the basic navigation bar, hyperlinks, and titles of the website.

--- code/flask_app.py
+++ code/flask_app.py
@@ -2,9 +2,29 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def w209():
-    file="about9.jpg"
-    return render_template("w209.html",file=file)
+#def w209():
+#    file="about9.jpg"
+#    return render_template("w209.html",file=file)
+
+def disasters_bar():
+    return render_template('disasters_bar.html')
+
+@app.route("/disasters_map")
+def disasters_map():
+    return render_template('disasters_map.html')
+
+@app.route("/time_line")
+def time_line():
+    return render_template('time_line.html')
+
+@app.route("/time_anim")
+def time_anim():
+    return render_template('time_anim.html')
+
+@app.route("/cost_bar")
+def cost_bar():
+    return render_template('cost_bar.html')
+
 
 if __name__ == "__main__":
     app.run()
