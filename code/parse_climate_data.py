0b4f2ffc315e954b6e389971b7189530046dc937
Tim <timothym98@ischool.berkeley.edu>
2024-07-29 15:38:03 -0700

    Data Cleaning
    Accessed and cleaned additional climate data: monthly & annual temperature by state, county, and annual temperature nationwide.

--- code/parse_climate_data.py
+++ code/parse_climate_data.py
@@ -6,6 +6,7 @@
 # Import directories
 from pathlib import Path
 import pandas as pd
+import numpy as np
 
 # Change wd to run on other machines.
 wd = Path("C:/Users/tmajidzadeh/Desktop/MIDS Notes/Datasci 209 (Data Visualization)/209_Majidzadeh_Sandoval_Venter/code")
@@ -27,10 +28,26 @@ schema = {
 
 
 types = ['min', 'max', 'avg']
-raw_data = {}
+raw_data_counties = {}
 for temp_type in types:
-	raw_data[temp_type] = pd.read_excel(wd/f'../data/raw/climdiv-{temp_type}-raw.xlsx', dtype=schema, engine='calamine')
+	raw_data_counties[temp_type] = pd.read_excel(wd/f'../data/raw/climdiv-{temp_type}-raw.xlsx', dtype=schema, engine='calamine')
 	# print(raw_data[temp_type].head())
+
+raw_data_states = {}
+for temp_type in types:
+	raw_data_states[temp_type] = pd.read_excel(wd/f'../data/raw/climdiv-state-{temp_type}-raw.xlsx', dtype=schema, engine='calamine')
+	# print(raw_data[temp_type].head())
+
+county_xwalk = pd.read_csv(
+	"C:/Users/tmajidzadeh/Desktop/MIDS Notes/Datasci 209 (Data Visualization)/209_Majidzadeh_Sandoval_Venter/data/ssa_fips_state_county_2023.csv",
+	dtype = {
+		'fipscounty': '|S',
+		'fy2023cbs': '|S',
+		'ssa_CODE': '|S'
+	}
+)
+county_xwalk = county_xwalk[['fipscounty', 'countyname_fips']]
+county_xwalk['countyname_fips'] = county_xwalk['countyname_fips'].str.capitalize() + " County"
 
 state_dict = {
 	"01": "Alabama",
@@ -82,6 +99,58 @@ state_dict = {
     "27": "New Hampshire",
     "28": "New Jersey",
     "29": "New Mexico",
+}
+
+state_dict_2 = {
+	"001": "Alabama",
+	"030": "New York",
+    "002": "Arizona",
+    "031": "North Carolina",
+    "003": "Arkansas",
+    "032": "North Dakota",
+    "004": "California",
+    "033": "Ohio",
+    "005": "Colorado",
+    "034": "Oklahoma",
+    "006": "Connecticut",
+    "035": "Oregon",
+    "007": "Delaware",
+    "036": "Pennsylvania",
+    "008": "Florida",
+    "037": "Rhode Island",
+    "009": "Georgia",
+    "038": "South Carolina",
+    "010": "Idaho",
+    "039": "South Dakota",
+    "011": "Illinois",
+    "040": "Tennessee",
+    "012": "Indiana",
+    "041": "Texas",
+    "013": "Iowa",
+    "042": "Utah",
+    "014": "Kansas",
+    "043": "Vermont",
+    "015": "Kentucky",
+    "044": "Virginia",
+    "016": "Louisiana",
+    "045": "Washington",
+    "017": "Maine",
+    "046": "West Virginia",
+    "018": "Maryland",
+    "047": "Wisconsin",
+    "019": "Massachusetts",
+    "048": "Wyoming",
+    "020": "Michigan",
+    "050": "Alaska",
+    "021": "Minnesota",
+    "022": "Mississippi",
+    "023": "Missouri",
+    "024": "Montana",
+    "025": "Nebraska",
+    "026": "Nevada",
+    "027": "New Hampshire",
+    "028": "New Jersey",
+    "029": "New Mexico",
 }
 
 fips_dict = {
@@ -161,19 +230,28 @@ element_dict = {
 	"28": "Minimum Temperature"
 }
 
-def process_data(df):
+def process_data(df, mode):
 	'''
 	Clean the data, reshape to long format and create human-readable columns and indexes.
 	Inputs:
 		Raw temperature data from the NCEI.
+		mode: 'county' for county data. Any other value for state data.
 	Outputs:
 		Long-format data with columns for year, month, state, county, and the temperature in farenheit.
 	'''
-	df['state_code'] = df['Column1'].str[:2]
-	df['county_fips'] = df['Column1'].str[2:5]
-	df['element_code'] = df['Column1'].str[5:7]
-	df['year'] = df['Column1'].str[7:].astype(int)
-	df['state'] = df['state_code'].map(state_dict)
+	if mode == 'county':
+		df['state_code'] = df['Column1'].str[:2]
+		df['county_fips'] = df['Column1'].str[2:5]
+		df['element_code'] = df['Column1'].str[5:7]
+		df['year'] = df['Column1'].str[7:].astype(int)
+		df['state'] = df['state_code'].map(state_dict)
+	else:
+		df['state_code'] = df['Column1'].str[:3]
+		df['county_fips'] = df['Column1'].str[3:4]
+		df['element_code'] = df['Column1'].str[4:6]
+		df['year'] = df['Column1'].str[6:].astype(int)
+		df['state'] = df['state_code'].map(state_dict_2)
+
 	df['element'] = df['element_code'].map(element_dict)
 	df.rename(
 		columns = {
@@ -193,25 +271,71 @@ def process_data(df):
 		},
 		inplace = True
 	)
-	df = df.melt(
-		id_vars=['raw_id', 'state_code', 'state', 'county_fips', 'element_code', 'element', 'year'],
-		value_vars=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-		var_name='month',
-		value_name='temp_farenheit'
-	)
+	if mode == 'county':
+		df = df.melt(
+			id_vars=['raw_id', 'state_code', 'state', 'county_fips', 'element_code', 'element', 'year'],
+			value_vars=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+			var_name='month',
+			value_name='temp_farenheit'
+		)
+	else:
+		df = df.melt(
+			id_vars=['raw_id', 'state_code', 'state', 'element_code', 'element', 'year'],
+			value_vars=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+			var_name='month',
+			value_name='temp_farenheit'
+		)
 	df['month_num'] = df['month'].map(month_dict)
 	df['country'] = "United States"
 	df['state_fips'] = df['state'].map(fips_dict)
-	df['full_fips'] = df['state_fips'] + df['county_fips']
+	
+	if mode == 'county':
+		df['fipscounty'] = df['state_fips'] + df['county_fips']
+		df = df.merge(
+			county_xwalk,
+			on = 'fipscounty',
+			how = 'left'
+		)
 
 	return df
 
-processed_data = {}
-for temp_type in raw_data.keys():
-	processed_data[temp_type] = process_data(raw_data[temp_type])
-	processed_data[temp_type].to_csv(wd/f'../data/monthly_county_{temp_type}_temps.csv', index=False)
+processed_data_counties, annual_data_counties = {}, {}
+for temp_type in raw_data_counties.keys():
+	processed_data_counties[temp_type] = process_data(raw_data_counties[temp_type], mode='county')
+	processed_data_counties[temp_type].to_csv(wd/f'../data/monthly_county_{temp_type}_temps.csv', index=False)
+
+	annual_data_counties[temp_type] = processed_data_counties[temp_type] \
+		.groupby(['country', 'state', 'state_fips', 'fipscounty', 'countyname_fips', 'element_code', 'element', 'year']) \
+		.agg({'temp_farenheit': np.nanmean})
+	annual_data_counties[temp_type].to_csv(wd/f'../data/annual_county_{temp_type}_temps.csv', index=True)
 
 concatenated = pd.concat(
-	[processed_data[temp_type] for temp_type in raw_data.keys()]
+	[processed_data_counties[temp_type] for temp_type in raw_data_counties.keys()]
 )
 concatenated.to_csv(wd/'../data/monthly_county_appended_temps.csv', index=False)
+
+concatenated = pd.concat(
+	[annual_data_counties[temp_type] for temp_type in raw_data_counties.keys()]
+)
+concatenated.to_csv(wd/'../data/annual_county_appended_temps.csv', index=True)
+
+
+processed_data_states, annual_data_states = {}, {}
+for temp_type in raw_data_states.keys():
+	processed_data_states[temp_type] = process_data(raw_data_states[temp_type], mode='state')
+	processed_data_states[temp_type].to_csv(wd/f'../data/monthly_state_{temp_type}_temps.csv', index=False)
+
+	annual_data_states[temp_type] = processed_data_states[temp_type] \
+		.groupby(['country', 'state', 'state_fips', 'element_code', 'element', 'year']) \
+		.agg({'temp_farenheit': np.nanmean})
+	annual_data_states[temp_type].to_csv(wd/f'../data/annual_state_{temp_type}_temps.csv', index=True)
+
+concatenated = pd.concat(
+	[processed_data_states[temp_type] for temp_type in raw_data_states.keys()]
+)
+concatenated.to_csv(wd/'../data/monthly_states_appended_temps.csv', index=False)
+
+concatenated = pd.concat(
+	[annual_data_states[temp_type] for temp_type in raw_data_states.keys()]
+)
+concatenated.to_csv(wd/'../data/annual_states_appended_temps.csv', index=True)
